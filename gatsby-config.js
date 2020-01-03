6cfb7f573fb90fc3ad9aff066bc59001a5f7063e
Antonio Puga <antoniopuga@Antonios-MacBook-Pro.local>
2020-01-03 09:01:07 +0100

    creation of gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+/**
+ * Configure your Gatsby Blog site with this file.
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Gatsby Boilerplate Starter Blog Base`,
+    siteUrl: `hhttps://github.com/cafeteando/gatsby-starter-blog-base`,
+    description: `Blazing fast modern Blog Starter for Gatsby`,
+  },
+}
