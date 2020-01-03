f2a65d0d551102b2472199a60f1c5984d899ebc5
Antonio Puga <antoniopuga@Antonios-MacBook-Pro.local>
2020-01-03 09:02:47 +0100

    new folders, src and static

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default () => <div>Wellcome to my Blog!</div>
+
+
+/*class Greeting extends React.Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+// Renders "Hello World":
+ReactDOM.render(
+  <Greeting />,
+  document.getElementById('example')
+);
+*/
